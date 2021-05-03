c159e80a04d43f77de62c3be5382a9fa2a7a4501
SergioEGGIT <sergioeg19@hotmail.com>
2021-05-02 23:21:11 -0600

    Fix Server

--- Server/src/index.js
+++ Server/src/index.js
@@ -16,34 +16,12 @@ Application.use(bodyParser.urlencoded({
 }));
 
 
-Application.all('*', function(req, res,next) {
-    /**
-     * Response settings
-     * @type {Object}
-     */
-    var responseSettings = {
-        "AccessControlAllowOrigin": req.headers.origin,
-        "AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
-        "AccessControlAllowMethods": "POST, GET, PUT, DELETE, OPTIONS",
-        "AccessControlAllowCredentials": true
-    };
-
-    /**
-     * Headers
-     */
-    res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
-    res.header("Access-Control-Allow-Origin",  responseSettings.AccessControlAllowOrigin);
-    res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
-    res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
-
-    if ('OPTIONS' == req.method) {
-        res.send(200);
+const server = require('http').createServer(Application);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
     }
-    else {
-        next();
-    }
-
-
 });
 
 // Ejectuar Routes 
