46bfd8050a7baff28cb2eac285628704f1a7be73
SergioEGGIT <sergioeg19@hotmail.com>
2021-05-02 23:18:07 -0600

    Fix Server

--- Server/src/index.js
+++ Server/src/index.js
@@ -8,13 +8,6 @@ const Application = Express();
 
 // Uses App 
 //enables cors
-Application.use(Cors({
-  'allowedHeaders': ['sessionId', 'Content-Type'],
-  'exposedHeaders': ['sessionId'],
-  'origin': '*',
-  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  'preflightContinue': false
-}));
 Application.use(bodyParser.json());
 Application.use(bodyParser.urlencoded({
 	
@@ -22,6 +15,36 @@ Application.use(bodyParser.urlencoded({
 	
 }));
 
+
+Application.all('*', function(req, res,next) {
+    /**
+     * Response settings
+     * @type {Object}
+     */
+    var responseSettings = {
+        "AccessControlAllowOrigin": req.headers.origin,
+        "AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
+        "AccessControlAllowMethods": "POST, GET, PUT, DELETE, OPTIONS",
+        "AccessControlAllowCredentials": true
+    };
+
+    /**
+     * Headers
+     */
+    res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
+    res.header("Access-Control-Allow-Origin",  responseSettings.AccessControlAllowOrigin);
+    res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
+    res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
+
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+
+
+});
 
 // Ejectuar Routes 
 Routes(Application);
